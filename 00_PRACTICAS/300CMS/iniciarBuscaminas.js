1d6cede7f1cf4575dd93afda9d8bebed702584b2
Mallen <cmallen@educa.madrid.org>
2023-11-27 10:16:44 +0100

    buscar tablero

--- 00_PRACTICAS/300CMS/iniciarBuscaminas.js
+++ 00_PRACTICAS/300CMS/iniciarBuscaminas.js
@@ -1,6 +1,49 @@
-// ---- FUNCIONES-------
+// ---- LÓGICA -------
 
-function dibujarTableroHTML(){
+
+function elegirNivel(){
+  let filas,columnas,dificultad;
+  dificultad=document.getElementById("nivel").value; 
+  //según la dificultad seleccionada 
+
+  switch (dificultad) {
+    case "FÁCIL":
+      filas=columnas=9;
+      document.write(dibujarTableroHTML(filas,columnas));
+      break;
+  
+    case "MEDIO":
+      filas=columnas=13;
+      document.write(dibujarTableroHTML(filas,columnas));
+      break;
+  
+    case "DIFÍCIL":
+      filas=columnas=19;
+      document.write(dibujarTableroHTML(filas,columnas));
+      break;
+  }
+  
+}
+
+
+
+
+// ---- FUNCIONES -------
+
+function dibujarTableroHTML(filas,columnas){
+
+let tablero="<table border=1>"
+
+for (let indiceFila = 0; indiceFila < filas; indiceFila++) {
+   tablero+="\n<tr>"
+  for (let inidiceColumna = 0; inidiceColumna < columnas; inidiceColumna++) {
+    tablero+="\n<td>"+indiceFila+"-"+inidiceColumna+"</td>";
+   }
+   tablero+="\n</tr>";
+}
+tablero+="\n</table>";
+
+return tablero;
 
 }//dibujarTableroHTML
 
