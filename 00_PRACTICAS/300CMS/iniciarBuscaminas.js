6f5927cb1c139ad516936dfeb40462869710e987
Mallen <cmallen@educa.madrid.org>
2024-01-10 13:16:34 +0100

    pintar tablero y css de botones

--- 00_PRACTICAS/300CMS/iniciarBuscaminas.js
+++ 00_PRACTICAS/300CMS/iniciarBuscaminas.js
@@ -1,94 +1,71 @@
 // ---- LÓGICA -------
 
-window.addEventListener("load",inicio);
+window.addEventListener("load", inicio, false);
 
-function inicio(){
-  document.getElementById("ok").onclick(elegirNivel);
-}
+function inicio() {
+  document.getElementById("ok").addEventListener("submit", elegirNivel, false);
+} //inicio
 
-
-function elegirNivel(){
-  let filas,columnas,dificultad;
-  dificultad=document.getElementById("nivel").value;
-  
-
-  //según la dificultad seleccionada 
+function elegirNivel(evento) {
+  evento.preventDefault();
+  let filas, columnas, dificultad;
+  dificultad = document.getElementById("nivel").value;
+  //según la dificultad seleccionada
 
   switch (dificultad) {
     case "FÁCIL":
-      filas=columnas=9
-      dibujarTableroHTML(filas,columnas);
+      filas = columnas = 9;
+      dibujarTableroHTML(filas, columnas);
       break;
 
     case "MEDIO":
-      filas=columnas=13;
-      document.write(dibujarTableroHTML(filas,columnas));
+      filas = columnas = 13;
+      dibujarTableroHTML(filas, columnas);
       break;
-  
+
     case "DIFÍCIL":
-      filas=columnas=19;
-      document.write(dibujarTableroHTML(filas,columnas));
-      break;
-  } 
-}
-// ---- FUNCIONES -------
-
-function dibujarTableroHTML(filas,columnas){
-
-let tablero=document.getElementById("tablero"); //etiqueta donde guardar el tablero
-let boton,intro;
-
-for (let indiceFila = 0; indiceFila < filas; indiceFila++) {
-  for (let inidiceColumna = 0; inidiceColumna < columnas; inidiceColumna++) {
-    boton=document.createElement("button");
-    boton.id=indiceFila+inidiceColumna;
-    tablero.appendChild(boton);
-    // boton+="<button id=\""+indiceFila+inidiceColumna+"\"> X </button>";
-   }
-   intro=document.createElement("br");
-   tablero.appendChild(intro);
-  //  boton+="\n</br>";
-}
-
-
-// return a;
-
-}//dibujarTableroHTML
-
-function generarTableroJS(){
-
-}//generarTableroJS
-
-
-
-function calcularNumMinas(x,y){
-
-  let dificultad;
-  dificultad=document.getElementById("nivel").value; 
-  switch (dificultad) {
-    case "FÁCIL":
-        
-      break;
-  
-    case "MEDIO":
-      
-      break;
-  
-    case "DIFÍCIL":
-      
+      filas = columnas = 19;
+      dibujarTableroHTML(filas, columnas);
       break;
   }
+}//elegirNivel
 
-}//calcularNumMinas
+// ---- FUNCIONES -------
 
-function numeroAleatorio(array,numeroMinas){
+function dibujarTableroHTML(filas, columnas) {
+  let tablero = document.getElementById("tablero"); //etiqueta Main donde guardar el tablero
+  let boton, intro;
 
+  for (let indiceFila = 0; indiceFila < filas; indiceFila++) {
+    for (let inidiceColumna = 0; inidiceColumna < columnas; inidiceColumna++) {
+      boton = document.createElement("button"); //Creamos el boton
+      boton.className = parseInt(indiceFila +""+ inidiceColumna); //Le damos una clase con su coordenada y la pasamos a numero entero
+      tablero.appendChild(boton); //Metemos el boton en el main
+    }
+    intro = document.createElement("br"); //Creamos un salto de linea
+    tablero.appendChild(intro); //Introducimos un salto de linea
+  }
+}//dibujarTableroHTML
 
+function generarTableroJS() {} //generarTableroJS
+
+function calcularNumMinas(x, y) {
+  let dificultad;
+  dificultad = document.getElementById("nivel").value;
+  switch (dificultad) {
+    case "FÁCIL":
+      break;
+
+    case "MEDIO":
+      break;
+
+    case "DIFÍCIL":
+      break;
+  }
+} //calcularNumMinas
+
+function numeroAleatorio(array, numeroMinas) {
   Math.floor(Math.random());
+} //numeroAleatorio
 
-}//numeroAleatorio
-
-function colocarBombasTableroJS(){
-
-}//colocarBombasTableroJS
-
+function colocarBombasTableroJS() {} //colocarBombasTableroJS
