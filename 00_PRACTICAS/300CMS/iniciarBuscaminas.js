f1e03ac450cb545e2fe60e4e6e0471b25fbdf4f4
MACMallen <MACMallen@gmail.com>
2024-02-02 21:27:52 +0100

    arreglado bug en array y funcionalidad de banderillas agregada

--- 00_PRACTICAS/300CMS/iniciarBuscaminas.js
+++ 00_PRACTICAS/300CMS/iniciarBuscaminas.js
@@ -1,15 +1,39 @@
-// ---- LÓGICA -------
 
-window.addEventListener("load", inicio, false);
+//Esperamos a que cargue la página para ejecutar el código
+window.addEventListener("load", inicio);
+
+
+// ------------ LISTENERS ------------ \\
 
 function inicio() {
-  document.getElementById("ok").addEventListener("submit", elegirNivel, false);
+  document.getElementById("ok").addEventListener("submit", elegirNivel);         //---> elegir nivel
+  document.getElementById("tablero").addEventListener("click", ponerBandera);    //---> PONER BANDERA
+  //document.getElementById("tablero").addEventListener("click", descubrirCasilla);
 } //inicio
 
+
+// ------------ TEST ------------ \\
+
+function prueba(e){
+  console.log("prueba");
+  console.log(e);
+  console.log(e.target);
+
+}
+
+
+// ------------ VARIABLES GLOBALES ------------ \\
 
 //creamos un array para guardar los atributos de cada casilla del tablero
 let arrayTablero = new Array();
 let lado;
+
+
+
+
+
+// ------------ FUNCIONES ------------ \\
+
 
 function elegirNivel(evento) {
   evento.preventDefault(); //evitamos que al enviar el formulario se refresque la página
@@ -19,8 +43,11 @@ function elegirNivel(evento) {
     document.querySelector('input').value = "RESETEAR";
     let dificultad;
     dificultad = document.getElementById("nivel").value;
-    //según la dificultad seleccionada
-
+    
+    //vaciamos el array para que no se acumulen los valores del tablero anterior
+    arrayTablero=[]; 
+    
+    //según la dificultad seleccionada tenemos un tamaño de tablero
     switch (dificultad) {
       case "FÁCIL":
         lado = 9;
@@ -43,28 +70,30 @@ function elegirNivel(evento) {
     console.log(arrayTablero);
   }
 
-}//elegirNivel
+}//elegirNivel(evento)
 
-// ---- FUNCIONES -------
+
+
+
 
 function dibujarTableroHTML(lado) {
 
 
   let boton, intro;
   let tablero = document.getElementById("tablero"); //recogemos la etiqueta main donde guardar el tablero
-  tablero.remove(); //eliminamos el contenido por si ya hubiera un tablero
-  tablero = document.createElement("main"); //creamos la etiqueta main
-  tablero.id = "tablero"; //asignamos a main el id=tablero
-  document.body.appendChild(tablero); //agregamos la etiqueta al body
+  
+  while (tablero.firstChild) {
+    tablero.removeChild(tablero.firstChild);
+  }
 
   for (let indiceFila = 0; indiceFila < lado; indiceFila++) {
     for (let inidiceColumna = 0; inidiceColumna < lado; inidiceColumna++) {
       boton = document.createElement("button"); //Creamos el boton
-      boton.className = indiceFila + " " + inidiceColumna; //Le damos una clase con su coordenada y la pasamos a numero entero
+      boton.id = indiceFila + " " + inidiceColumna; //Le damos una id con su coordenada y la pasamos a numero entero
       tablero.appendChild(boton); //Metemos el boton en el main
 
       //A la vez que creamos el tablero, generamos un array acorde a su tamaño
-      //Metemos un objeto en el array por cada posición del tablero con identicas propiedades (salvo la posiciónX y posiciónY) 
+      //Metemos un objeto en el array por cada botón del tablero con las misma propiedades (salvo la posición) 
       arrayTablero.push({ 
         posicionX: indiceFila, 
         posicionY: inidiceColumna, 
@@ -77,28 +106,68 @@ function dibujarTableroHTML(lado) {
     intro = document.createElement("br"); //Creamos un salto de linea
     tablero.appendChild(intro); //Introducimos el salto de linea
   }
-}//dibujarTableroHTML
+}//dibujarTableroHTML(tablero)
+
+
+
 
 function colocarMinas(numMinas) {
 
+  //crearemos una coordenada X e Y aleatoria en funcion del numero de minas
   for (let i = 0; i < numMinas;) {
+
+    //Creamos coordneadas aleatorias usando el lado como limite
     let x = Math.floor(Math.random() * lado);
     let y = Math.floor(Math.random() * lado);
 
-    //comprobamos si la posicion aleatoria ya tiene una bomba
-    let casilla = arrayTablero.find(casilla => casilla.posicionX === x && casilla.posicionY === y);
+    //comprobamos si la posición aleatoria existe y si ya tiene una bomba
+    let casilla = encontrarCasilla(x, y);
 
     if (casilla && casilla.bomba===false) {
-      i++;
+      i++; //el FOR solo avanza cuando se ha colocado una mina en una casilla correcta
       casilla.bomba=true;
-      
+      document.getElementById(x + " " + y).className = "bomba";
       // console.log(casilla);
     }
   }
-} //colocarMinas
+} //colocarMinas(numMinas)
+
+
 
 
 function numeroMinas() {
   //El numero de minas será el 13% del tamaño del tablero
   return (arrayTablero.length * 0.13).toFixed(0);
-} //numeroMinas
+} //numeroMinas()
+
+
+
+function encontrarCasilla(x, y) {
+  //segun el id del boton que pulsen, buscamos esa posicion en el array
+  return arrayTablero.find(casilla => casilla.posicionX === x && casilla.posicionY === y);
+}//encontrarCasilla(x, y)
+
+
+
+function ponerBandera(evento) {
+
+  //evitamos que salga el menú contextual
+  evento.preventDefault();
+
+  //recojemos la posición del botón que ha sido pulsado
+  let boton=evento.target.id.split(" ");
+
+  //buscamos el botón en el array
+  let casilla=encontrarCasilla(parseInt(boton[0]), parseInt(boton[1]));
+  
+  //Si la casilla no tiene bandera se pone
+  if(casilla && casilla.banderilla==false){
+    casilla.banderilla = true;
+    evento.target.classList.toggle("bandera");
+
+  //Si ya hay una bandera se quita  
+  }else{
+    casilla.banderilla = false;
+    evento.target.classList.toggle("bandera");
+  }
+}//ponerBandera(evento)
