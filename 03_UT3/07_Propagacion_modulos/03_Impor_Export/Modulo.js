2a5441cd4231e4b68770849992b0f492562c62ca
Mallen <cmallen@educa.madrid.org>
2024-02-07 10:08:14 +0100

    propagacion y poke api

---
+++ 03_UT3/07_Propagacion_modulos/03_Impor_Export/Modulo.js
@@ -0,0 +1,6 @@
+import {cadenaMayuscula} from './funciones.js';
+
+const saludo = cadenaMayuscula('hola , bienvenida a mi presentacion del libro luna de pluton , easpero te guste');
+console.log(saludo);
+
+import {NUMEROPI, NUMEROE, NUMEROCERO} from './funciones.js';
---
+++ 03_UT3/07_Propagacion_modulos/03_Impor_Export/funciones.js
@@ -0,0 +1,7 @@
+export const cadenaMayuscula = str => str.toUpperCase();// convierte en mayusculas
+
+const NUMEROPI = 3.1416;
+const NUMEROE = 2.7182;
+const NUMEROCERO = 0.0000;
+
+export{NUMEROPI, NUMEROE, NUMEROCERO}; // exporta las constantes
